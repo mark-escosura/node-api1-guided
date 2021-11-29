a830422150763caea6d69d69df3c8d8e56719213
mark-escosura <escosuramarkse@gmail.com>
2021-11-29 15:25:28 -0800

    put endpoint successfully created

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ server.get("/api/dogs", (req, res) => {
     .catch((err) => {
       res
         .status(500)
-        .json({ message: "something bad happen", err: err.message });
+        .json({ message: "error getting all dogs", err: err.message });
     });
 });
 
@@ -42,9 +42,64 @@ server.get("/api/dogs", (req, res) => {
 // })
 
 // [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
-server.get("/api/dogs");
+server.get("/api/dogs/:id", async (req, res) => {
+  try {
+    const dog = await Dog.findById(req.params.id);
+    if (!dog) {
+      res.status(404).json({
+        message: `dog by id ${req.params.id} does not exist`
+      })
+    } else {
+      res.json(dog);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error getting dog by id",
+      error: err.message,
+    });
+  }
+});
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
+server.post("/api/dogs", async (req, res) => {
+  try {
+    // pull dog info from req.body
+    // send back to client the new dog
+    if (!req.body.name || !req.body.weight) {
+      res.status(400).json({
+        message: "name and weight are required",
+      });
+    } else {
+      // use Dog.create with req.body
+      const newDog = await Dog.create(req.body);
+      res.status(201).json(newDog);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error posting new dog",
+      error: err.message,
+    });
+  }
+});
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
+server.put('/api/dogs/:id', async (req, res) => {
+  const { id } = req.params
+  const { body }= req
+  try {
+    const updated = await Dog.update(id, body)
+    if (!updated) {
+      res.status(404).json({
+        message: `dog by id ${id} does not exists`,
+      })
+    } else {
+      res.json(updated)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error updating existing dog",
+      error: err. message
+    })
+  }
+})
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
