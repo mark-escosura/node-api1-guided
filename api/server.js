5e12a918485d7e598a9e9426ba9e71cee3d9fa9d
mark-escosura <escosuramarkse@gmail.com>
2021-11-29 11:01:25 -0800

    endpoint for getAllDogs

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 // IMPORTS AT THE TOP
 const express = require("express");
-
+const Dog = require("./dog-model");
 
 // INSTANCE OF EXPRESS APP
 const server = express();
 
 // GLOBAL MIDDLEWARE
+server.use(express.json());
 
 // ENDPOINTS
 
@@ -14,10 +15,37 @@ server.get("/hello", (req, res) => {
   res.json({ message: "hello" });
 });
 // [GET] /api/dogs (R of CRUD, fetch all dogs)
+server.get("/api/dogs", (req, res) => {
+  // pull dogs from database
+  Dog.findAll()
+    .then((dogs) => {
+      // send dogs back to client
+      res.json(dogs);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "something bad happen", err: err.message });
+    });
+});
+
+// server.get('/api/dogs', async (req, res) => {
+//     try {
+//         const dogs = await Dog.findAll()
+//         res.json(dogs)
+//     } catch (err) {
+//         res.status(500).json({
+//             message: 'something bad happened',
+//             error: err.message
+//         })
+//     }
+// })
+
 // [GET] /api/dogs/:id (R of CRUD, fetch dog by :id)
+server.get("/api/dogs");
 // [POST] /api/dogs (C of CRUD, create new dog from JSON payload)
 // [PUT] /api/dogs/:id (U of CRUD, update dog with :id using JSON payload)
 // [DELETE] /api/dogs/:id (D of CRUD, remove dog with :id)
 
 // EXPOSING THE SERVER TO OTHER MODULES
-module.exports = server
+module.exports = server;
