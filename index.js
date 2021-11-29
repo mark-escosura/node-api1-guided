19a92d3a888bb89f51c3cffcf33732ec8930b25b
mark-escosura <escosuramarkse@gmail.com>
2021-11-29 10:36:36 -0800

    Third Commit

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 // import the server and start it!
-console.log("Hello World")
+
+const server = require('./api/server')
+
+server.listen(9000, () => {
+  console.log("server listening on 9000");
+});
